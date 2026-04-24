d94c55c533c20584053402f3815995deb69b6c5d
estjuanitaizambr1-glitch <est.juanitai.zambr1@unimilitar.edu.co>
2026-04-24 09:25:23 -0500

    Estructura base HTML5 y configuración del Canvas

---
+++ parcial2.js
@@ -0,0 +1,8 @@
+/**
+ * Universidad Militar Nueva Granada- Ingeniería en Multimedia
+ * Asignatura: Introducción a la Computación Gráfica
+ * Estudiante: Juanita Isabel Zambrano Maldonado
+ */
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
